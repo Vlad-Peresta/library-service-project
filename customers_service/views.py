97a2e462eb160ddd255fc61e960b3641e6f6eec2
Vladyslav Peresta <perestavlad@gmail.com>
2023-01-03 00:06:00 +0200

    Add views to the app

--- customers_service/views.py
+++ customers_service/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from customers_service.serializers import UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
