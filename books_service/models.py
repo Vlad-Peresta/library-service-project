c347ff8622969bce4b9e285c39bc52126ec71cac
Vladyslav Peresta <perestavlad@gmail.com>
2023-01-02 23:24:56 +0200

    Add Book model and migrations

--- books_service/models.py
+++ books_service/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Cover(models.TextChoices):
+    HARD = "Hard", _("Hard")
+    SOFT = "Soft", _("Soft")
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=Cover.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
