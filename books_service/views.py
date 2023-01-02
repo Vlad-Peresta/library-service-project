35b89fcfb1dd9c1f5cee4aed074cd1bd2486f854
Vladyslav Peresta <perestavlad@gmail.com>
2023-01-02 23:31:31 +0200

    Add BookViewSet to the `books_service` app

--- books_service/views.py
+++ books_service/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books_service.models import Book
+from books_service.permissions import IsAdminOrReadOnly
+from books_service.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
