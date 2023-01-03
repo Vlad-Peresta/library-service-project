3b4e272885c45745afa95d8e764bc7effae88cba
Vladyslav Peresta <perestavlad@gmail.com>
2023-01-03 22:53:06 +0200

    Add BorrowingCreateSerializer with validations

--- borrowings_service/serializers.py
+++ borrowings_service/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from books_service.serializers import BookSerializer
@@ -17,3 +18,42 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+        )
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            book = validated_data.get("book")
+            borrowing = Borrowing.objects.create(**validated_data)
+            book.inventory -= 1
+            book.save()
+
+            return borrowing
+
+    def validate(self, attrs):
+        data = super().validate(attrs=attrs)
+        Borrowing.validate_dates(
+            attrs.get("borrow_date"),
+            attrs.get("expected_return_date"),
+            attrs.get("actual_return_date"),
+            serializers.ValidationError
+        )
+
+        return data
+
+    def validate_book(self, value):
+        if value.inventory == 0:
+            raise serializers.ValidationError(
+                "Such book is not available in the library"
+            )
+        return value
