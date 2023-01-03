91f86f9cde5e0c70e931e790563586e3ca73e1e4
Vladyslav Peresta <perestavlad@gmail.com>
2023-01-03 17:38:19 +0200

    Add constraints to the Borrowing model

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -1,7 +1,9 @@
 from datetime import date
 
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from books_service.models import Book
 
@@ -11,15 +13,27 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField()
     book = models.ForeignKey(
-        Book,
-        on_delete=models.CASCADE,
-        related_name="borrowings"
+        Book, on_delete=models.CASCADE, related_name="borrowings"
     )
     user = models.ForeignKey(
-        get_user_model(),
-        on_delete=models.CASCADE,
-        related_name="borrowings"
+        get_user_model(), on_delete=models.CASCADE, related_name="borrowings"
     )
+
+    def clean(self):
+        if self.borrow_date > self.expected_return_date:
+            raise ValidationError(
+                _("Borrow date should not be later than expected return date")
+            )
+
+        if self.borrow_date > self.actual_return_date:
+            raise ValidationError(
+                _("Borrow date should not be later than actual return date")
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.user} borrowed {self.book} {self.borrow_date}"
