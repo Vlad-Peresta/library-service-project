7c49d98fed6c7f580ba753746a15a6b00221c22d
Vladyslav Peresta <perestavlad@gmail.com>
2023-01-06 20:14:35 +0200

    Add secret key to the project

--- library_service/settings.py
+++ library_service/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uq@y7bz)mdi@iatrjjl8crbwrv=(yuto@&s+hcb5#x72io4&j@"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
